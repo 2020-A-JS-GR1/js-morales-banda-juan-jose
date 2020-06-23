1897909c98398d0cf2a0767f6c2d566c91ac3135
Juan José Morales <juan.morales01@epn.edu.ec>
2020-06-23 18:06:44 -0500

    Fin de operadores y de funciones

--- 01-JavaScript/03-operadores.js
+++ 01-JavaScript/03-operadores.js
@@ -51,6 +51,59 @@ const arreglo = [
     },
 ]
 
+const arreglo2 = [
+    {
+        id: 1,
+        nombre: 'Adrian',
+        nota: 5
+    },
+    {
+        id: 2,
+        nombre: 'Vicente',
+        nota: 8
+    },
+    {
+        id: 3,
+        nombre: 'Carolina',
+        nota: 14
+    },
+    {
+        id: 4,
+        nombre: 'Wendy',
+        nota: 16
+    },
+    {
+        id: 5,
+        nombre: 'Andrea',
+        nota: 19
+    },
+    {
+        id: 6,
+        nombre: 'Pamela',
+        nota: 19
+    },
+    {
+        id: 7,
+        nombre: 'Cristian',
+        nota: 20
+    },
+    {
+        id: 8,
+        nombre: 'Daniel',
+        nota: 19
+    },
+    {
+        id: 9,
+        nombre: 'Lilly',
+        nota: 14
+    },
+    {
+        id: 10,
+        nombre: 'Ramiro',
+        nota: 12
+    }
+];
+
 // Buscar dentro de un arreglo
 // Funciones como parámetros
 
@@ -139,9 +192,57 @@ const respuestaFilter = arreglo.filter(
 console.log('respuestaFilter', respuestaFilter);
 console.log("arreglo", arreglo)
 
-
-
 // DEBER:
 /*
 usar map y filter
 */
+
+// SOME -> expresion
+// DEVUELVE BOOLEAN
+// Devuelve true cuando hay almenos un valor que cumpla
+// ¿Hay ALGUNA nota menor a nueve? SI o NO
+// OR
+const respuestaSome = arreglo2.some(
+    (valorActual, indiceActual, arregloCompleto) => {
+        return valorActual.nota < 9;
+    }
+);
+console.log('arreglo2', arreglo2)
+console.log('respuestaSome', respuestaSome);
+
+
+// EVERY -> expresión
+// DEVUELVE UN BOOLEAN
+// ¿TODAS las notas son mayores a 14? SI NO
+// AND
+const respuestaEvery = arreglo2.every(
+    (valorActual, indiceActual, arregloCompleto) => {
+        return valorActual.nota > 14;
+    }
+);
+console.log('arreglo2', arreglo2);
+console.log('respuestaEvery', respuestaEvery);
+
+// Reduce
+// reduce       izq -> der
+// reduceRight  der -> izq
+const respuestaReduce = arreglo2.reduce(
+    //valorAcumulado, valorSActual, indice, arreglo
+    (valorAcumulado, valorActual, indice, arreglo) => {
+        return valorAcumulado + valorActual.nota;
+    },
+    0  // Acumulador
+)
+console.log('respuestaReduce', respuestaReduce)  // 146
+
+// EJEMPLOS CON EL ACUMULADOR
+
+const arregloEstudiantesMenoresANueve = arreglo2
+    .map((valorActual) => valorActual.nota * 1.3)  // añadiendo el 30%
+    .filter((nota) => nota < 9)  // busco a los < 9
+const totalPuntosEstudiantes = arregloEstudiantesMenoresANueve
+    .reduce((acumulado, actual) => acumulado + actual, 0)  // total
+const notaPromedio = totalPuntosEstudiantes / arregloEstudiantesMenoresANueve.length
+console.log('notaPromedio', notaPromedio)
+
+
