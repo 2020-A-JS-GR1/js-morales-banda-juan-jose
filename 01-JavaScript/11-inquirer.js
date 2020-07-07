3537845596991f89263b93777e6b70fb05b877cf
Juan José Morales <juan.morales01@epn.edu.ec>
2020-07-07 18:02:22 -0500

    Fin de promesas, inquirer

---
+++ 01-JavaScript/11-inquirer.js
@@ -0,0 +1,24 @@
+const inquirer = require('inquirer');
+
+async function main() {
+    try {
+        const respuesta = await inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'nombre',
+                    message: 'Ingresa tu nombre'
+                },
+                {
+                    type: 'input',
+                    name: 'edad',
+                    message: 'Ingresa tu edad'
+                },
+            ]);
+        console.log('Respuesta', respuesta);
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+main();
